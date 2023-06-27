d7b2d35fea0e7aaaf7979fa238c266e271cdd951
abdullinda <105074853+abdullinda@users.noreply.github.com>
2023-06-27 12:47:38 +0300

    client.services.js

--- services/client.services.js
+++ services/client.services.js
@@ -5,30 +5,30 @@ const app = express();
 const client = new MongoClient('mongodb://127.0.0.1:27017');
 const db = client.db('AH20231CP1');
 
-async function createCliente(cliente) {
+async function createClient(cliente) {
   await client.connect();
   const result = await db.collection("Projects").insertOne(cliente);
   return result.ops[0];
 }
 
-async function editCliente(id, cliente) {
+async function editClient(id, cliente) {
   await client.connect();
   const result = await db.collection("Projects").updateOne({ _id: new ObjectId(id) }, { $set: cliente });
   return result.modifiedCount > 0;
 }
 
-async function replaceCliente(id, cliente) {
+async function replaceClient(id, cliente) {
   await client.connect();
   const result = await db.collection("Projects").replaceOne({ _id: new ObjectId(id) }, cliente);
   return result.modifiedCount > 0;
 }
-async function deleteCliente(id) {
+async function deleteClient(id) {
   await client.connect();
   const result = await db.collection("Projects").deleteOne({ _id: new ObjectId(id) });
   return result.deletedCount > 0;
 }
 
-async function getClientes(filter = {}) {
+async function getClient(filter = {}) {
   await client.connect();
   const filterMongo = { deleted: { $ne: true } };
   if (filter?.cliente) {
@@ -37,12 +37,12 @@ async function getClientes(filter = {}) {
   return db.collection("Projects").find(filterMongo).toArray();
 }
 
-async function getClienteById(id) {
+async function getClientById(id) {
   await client.connect();
   return db.collection("Projects").findOne({ _id: new ObjectId(id) });
 }
 
-async function getClientesBySection(section) {
+async function getClientBySection(section) {
   await client.connect();
   const filter = { section: section.toLowerCase() };
   return db.collection("Projects").find(filter).toArray();
@@ -50,11 +50,12 @@ async function getClientesBySection(section) {
 
 
 export {
-  getClientes,
-  getClienteById,
-  createCliente,
-  editCliente,
-  deleteCliente,
-  replaceCliente,
-  getClientesBySection
+  getClient,
+  getClientById,
+  createClient,
+  editClient,
+  deleteClient,
+  replaceClient,
+  getClientBySection
 };
+
